2417ef9ad31c6dec13eb9d3a94cfddcebccb683a
Steve <Steve@Steves-MacBook-Pro.local>
2022-06-23 07:22:01 +0200

    task 3&4

--- utils/task3.py
+++ utils/task3.py
@@ -1,3 +1,5 @@
+from dataclasses import replace
+from secrets import choice
 import numpy as np
 
 from .task1 import get_iou
@@ -33,4 +35,97 @@ def sample_proposals(pred, target, xyz, feat, config, train=False):
         config['num_fg_sample'] maximum allowed number of foreground samples
         config['bg_hard_ratio'] background hard difficulty ratio (#hard samples/ #background samples)
     '''
-    pass
+    co_iou = get_iou(pred, target)
+    iou_target = np.max(co_iou, axis=1) #(N,)
+    iou_target_index = np.argmax(co_iou, axis=1) #(N,)
+
+    if not train:
+        assigned_targets = target[iou_target_index] #(N,7)
+        return assigned_targets, xyz, feat, iou_target
+    else:
+        fg_cr1_index = np.where(iou_target >= config['t_fg_lb'])[0]
+        fg_cr1_num = fg_cr1_index.size
+
+        fg_cr2_num = target.shape[0] #M
+        iou_pred = np.max(co_iou, axis=0) #(fg_cr2_num,) w.r.t. pred
+        iou_pred_index = np.argmax(co_iou, axis=0) #(fg_cr2_num,) pred
+
+        fg_num = fg_cr1_num + fg_cr2_num
+
+
+        bg_mask = iou_target < config['t_bg_up']
+        bg_easy_mask = iou_target < config['t_bg_hard_lb']
+        bg_hard_mask = bg_mask & ~bg_easy_mask
+        bg_easy_index = np.where(bg_easy_mask)[0]
+        bg_hard_index = np.where(bg_hard_mask)[0]
+        bg_easy_num = bg_easy_index.size
+        bg_hard_num = bg_hard_index.size
+        bg_num = bg_easy_num + bg_hard_num
+
+        fg_cr1_choice = np.array([])
+        fg_cr2_choice = np.array([])
+        bg_easy_choice = np.array([])
+        bg_hard_choice = np.array([])
+
+        if bg_num == 0:
+            fg_cr1_choice, fg_cr2_choice = sample_fg(64, fg_cr1_num, fg_cr2_num)
+        elif fg_num == 0:
+            bg_easy_choice, bg_hard_choice = sample_bg(64, bg_easy_num, bg_hard_num, config['bg_hard_ratio'])
+        else:
+            num_fg_sample = np.min(fg_num,config['num_fg_sample'])
+            fg_cr1_choice, fg_cr2_choice = sample_fg(num_fg_sample, fg_cr1_num, fg_cr2_num)
+            bg_easy_choice, bg_hard_choice = sample_bg(64-num_fg_sample, bg_easy_num, bg_hard_num, config['bg_hard_ratio'])
+        
+        assigned_targets = np.vstack([target[iou_target_index[fg_cr1_index][fg_cr1_choice]],
+                                      target[fg_cr2_choice],
+                                      target[iou_target_index[bg_easy_index][bg_easy_choice]],
+                                      target[iou_target_index[bg_hard_index][bg_hard_choice]]])
+
+        xyz_ret = np.vstack([xyz[fg_cr1_index[fg_cr1_choice]],
+                             xyz[iou_pred_index[fg_cr2_choice]],
+                             xyz[bg_easy_index[bg_easy_choice]],
+                             xyz[bg_hard_index[bg_hard_choice]]])
+
+        feat_ret = np.vstack([feat[fg_cr1_index[fg_cr1_choice]],
+                              feat[iou_pred_index[fg_cr2_choice]],
+                              feat[bg_easy_index[bg_easy_choice]],
+                              feat[bg_hard_index[bg_hard_choice]]])
+
+        iou_ret = np.vstack([iou_target[fg_cr1_index[fg_cr1_choice]],
+                             iou_pred[fg_cr2_choice],
+                             iou_target[bg_easy_index[bg_easy_choice]],
+                             iou_target[bg_hard_index[bg_hard_choice]]])
+
+        return assigned_targets, xyz_ret, feat_ret, iou_ret
+
+def sample_fg(num_needed, fg_cr1_num, fg_cr2_num):
+    choice = sample_w_padding(fg_cr1_num+fg_cr2_num, num_needed)
+    fg_cr1_choice = choice[choice<fg_cr1_num]
+    fg_cr2_choice = choice[choice>=fg_cr1_num] - fg_cr1_num
+    return np.sort(fg_cr1_choice), np.sort(fg_cr2_choice)
+
+
+def sample_bg(num_needed, bg_easy_num, bg_hard_num, bg_hard_ratio):
+    bg_easy_choice = np.array([])
+    bg_hard_choice = np.array([])
+
+    if bg_easy_num==0:
+        bg_hard_choice = sample_w_padding(bg_hard_num, num_needed)
+    elif bg_hard_num==0:
+        bg_easy_choice = sample_w_padding(bg_easy_num, num_needed)
+    else:
+        bg_hard_sample_num = np.floor(bg_hard_ratio * num_needed).astype(int)
+        bg_hard_choice = sample_w_padding(bg_hard_num, bg_hard_sample_num)
+        bg_easy_choice = sample_w_padding(bg_easy_num, num_needed-bg_hard_sample_num)
+
+    return np.sort(bg_easy_choice), np.sort(bg_hard_choice)
+
+
+def sample_w_padding(num_elements, num_needed):
+    if num_elements >= num_needed:
+        return np.random.choice(num_elements, size=num_needed, replace=False)
+    else:
+        choice = np.arange(num_elements)
+        repeat = np.random.choice(num_elements, size=num_needed-num_elements, replace=True)
+        return np.concatenate((choice, repeat))
+    
--- utils/task4.py
+++ utils/task4.py
@@ -22,12 +22,19 @@ class RegressionLoss(nn.Module):
         useful config hyperparameters
             self.config['positive_reg_lb'] lower bound for positive samples
         '''
-        pass
+        mask = iou >= self.config['positive_reg_lb']
+        pred = pred[mask]
+        target = target[mask]
+        loss_loc = self.loss(pred[:,:3], target[:,:3])
+        loss_size = self.loss(pred[:,3:6], target[:,3:6])
+        loss_rot = self.loss(pred[:,6], target[:,6])
+        return loss_loc + 3*loss_size + loss_rot
 
 class ClassificationLoss(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
+        self.m = nn.Sigmoid()
         self.loss = nn.BCELoss()
 
     def forward(self, pred, iou):
@@ -38,10 +45,22 @@ class ClassificationLoss(nn.Module):
         avoid incorrect training signals to supervise our network.  A proposal
         is considered as positive (class 1) if its maximum IoU with ground
         truth boxes is ≥ 0.6, and negative (class 0) if its maximum IoU ≤ 0.45.
-            pred (N,7) predicted bounding boxes
+            pred (N,1) predicted bounding boxes
             iou (N,) initial IoU of all paired proposal-targets
         useful config hyperparameters
             self.config['positive_cls_lb'] lower bound for positive samples
             self.config['negative_cls_ub'] upper bound for negative samples
         '''
-        pass
+        N = pred.size(0)
+        target = torch.zeros(N,1)
+        
+        mask_pos = iou >= self.config['positive_cls_lb']
+        mask_neg = iou <= self.config['negative_cls_ub']
+        mask = mask_pos | mask_neg
+
+        target[mask_pos] = 1
+        target = target[mask]
+        pred = pred[mask]
+
+        loss = self.loss(pred, target)
+        return loss
