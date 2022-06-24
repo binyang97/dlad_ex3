8b4ea6c5cb83688742e8fce050deb5fe2c078da0
Steve <Steve@student-net-hg-0694.intern.ethz.ch>
2022-06-24 21:33:41 +0200

    task 2

--- utils/task2.py
+++ utils/task2.py
@@ -2,9 +2,6 @@ from logging.config import valid_ident
 import numpy as np
 from timeit import default_timer as timer
 
-
-from utils.task3 import sample_w_padding
-
 def roi_pool(pred, xyz, feat, config):
     '''
     Task 2
@@ -32,33 +29,38 @@ def roi_pool(pred, xyz, feat, config):
         config['delta'] extend the bounding box by delta on all sides (in meters)
         config['max_points'] number of points in the final sampled ROI
     '''
-    # pooled_xyz = []
-    # pooled_feat = []
-    # valid = []
+    start = timer()
     enlarged_pred = enlarge_box(pred, config['delta'])
+
+    valid_indices, valid = points_in_boxes(xyz, enlarged_pred, config['max_points'])
+    duration = timer() - start
+    print('boxes duration [ms]:  {:.1f}'.format(duration*1000))
 
     # for (i, box) in enumerate(enlarged_pred):
     #     xyz_index = points_in_box(xyz, box)
-    #     xyz_num = xyz_index.size
 
-    #     if xyz_num > 0:
-    #         xyz_choice = sample_w_padding(xyz_num, config['max_points'])
-    #         valid_index = xyz_index[xyz_choice]
+    #     if len(xyz_index) > 0:
+    #         valid_index = sample_w_padding(xyz_index, config['max_points'])
+    #         # pooled_xyz[i] = xyz[valid_indices]
+    #         # pooled_feat[i] = feat[valid_indices]
+    #         # pooled_xyz.append(xyz[valid_index])
+    #         # pooled_feat.append(feat[valid_index])
+    #         valid_indices.append(valid_index)
     #         valid.append(i)
-    #         pooled_xyz.append(xyz[valid_index])
-    #         pooled_feat.append(feat[valid_index])
-
+        
+    # valid_indices = np.concatenate(valid_indices)
+    # pooled_xyz = pooled_xyz[valid]
+    # pooled_feat = pooled_feat[valid]
+    # pooled_xyz = np.array(pooled_xyz)
+    # pooled_feat = np.array(pooled_xyz)
     # valid = np.array(valid)
-
-    pooled_xyz, pooled_feat, valid = points_in_boxes(xyz, feat, enlarged_pred, config['max_points'])
-
     start = timer()
     valid_pred = pred[valid]
-    # pooled_xyz = np.array([xyz[k] for k in valid_indices])
-    # pooled_feat = np.array([feat[k] for k in valid_indices])
+    pooled_xyz = xyz[valid_indices]
+    pooled_feat = feat[valid_indices]
 
     duration = timer() - start
-    print('main duration [ms]:  {:.1f}'.format(duration*1000))
+    print('indexing duration [ms]:  {:.1f}'.format(duration*1000))
 
     return valid_pred, pooled_xyz, pooled_feat
 
@@ -115,8 +117,7 @@ def points_in_box(xyz, box):
     duration = timer() - start
     print('crop duration [ms]:  {:.1f}'.format(duration*1000))
 
-    start = timer()
-    xyz_prime = np.zeros(xyz_sub.shape)
+    # xyz_prime = np.zeros(xyz_sub.shape)
     xyz_prime = np.dot(T_inv, xyz_sub)
     xyz_prime = xyz_prime.T
 
@@ -124,8 +125,8 @@ def points_in_box(xyz, box):
                      (xyz_prime[:,1] <= 0) & (xyz_prime[:,1] >= -h) & \
                      (xyz_prime[:,2] >= -w/2) & (xyz_prime[:,2] <= w/2)
 
-    xyz_prime_index = np.where(xyz_prime_mask)[0]
-    xyz_index = xyz_sub_index[xyz_prime_index]
+    # xyz_prime_index = np.where(xyz_prime_mask)[0]
+    xyz_index = xyz_sub_index[xyz_prime_mask]
     print(xyz_index.shape)
 
     duration = timer() - start
@@ -134,7 +135,7 @@ def points_in_box(xyz, box):
     return xyz_index
 
 
-def points_in_boxes(xyz, feat, boxes, max_points):
+def points_in_boxes(xyz, boxes, max_points):
     '''
     input
         xyz (N,3) points in rectified reference frame
@@ -144,24 +145,32 @@ def points_in_boxes(xyz, feat, boxes, max_points):
         valid (K') index vector showing valid bounding boxes, i.e. with at least
                    one point within the box
     '''
-    # valid_indices = []
-    pooled_xyz = []
-    pooled_feat = []
+    valid_indices = []
     valid = []
     for (i, box) in enumerate(boxes):
         xyz_index = points_in_box(xyz, box)
-        xyz_num = xyz_index.size
 
-        if xyz_num > 0:
-            xyz_choice = sample_w_padding(xyz_num, max_points)
-            valid_indices = xyz_index[xyz_choice]
-            pooled_xyz.append(xyz[valid_indices])
-            pooled_feat.append(feat[valid_indices])
-            # valid_indices.append(xyz_index[xyz_choice])
+        if len(xyz_index) > 0:
+            start = timer()
+            valid_index = sample_w_padding(xyz_index, max_points)
+            valid_indices.append(valid_index)
             valid.append(i)
-        
-    # valid_indices = np.vstack(valid_indices)
-    pooled_xyz = np.array(pooled_xyz)
-    pooled_feat = np.array(pooled_xyz)
+            duration = timer() - start
+            print('append duration [ms]:  {:.1f}'.format(duration*1000))
+
+    start = timer()
+    valid_indices = np.array(valid_indices)
     valid = np.array(valid)
-    return pooled_xyz, pooled_feat, valid
+
+    duration = timer() - start
+    print('to array duration [ms]:  {:.1f}'.format(duration*1000))
+    return valid_indices, valid
+
+def sample_w_padding(indices, num_needed):
+    num_elements = indices.size
+    if num_elements >= num_needed:
+        return np.random.choice(indices, size=num_needed, replace=False)
+    else:
+        choice = indices
+        repeat = np.random.choice(indices, size=num_needed-num_elements, replace=True)
+        return np.concatenate((choice, repeat))
